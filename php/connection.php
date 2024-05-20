981d01b14e409fc2397b27eaba7ca22b4528eaa5
farhan <farhanangga8900@gmail.com>
2024-05-21 02:25:40 +0700

    penyesuaian code

--- php/connection.php
+++ php/connection.php
@@ -4,11 +4,6 @@ $username = "root";
 $password = "";
 $dbname = "casepose";
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = mysqli_connect($servername, $username, $password, $dbname);
 
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
 ?>
