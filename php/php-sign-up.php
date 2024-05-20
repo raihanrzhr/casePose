4b4ecf0bddb0158dc2fa8fcd925f1e88f250214a
farhan <farhanangga8900@gmail.com>
2024-05-21 03:19:35 +0700

    peyesuaian logic userId

--- php/php-sign-up.php
+++ php/php-sign-up.php
@@ -29,16 +29,25 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 header("Location: ../sign-up.php?pesan=sinkron");
                 exit();
             } else {
-                // Hash kata sandi sebelum disimpan ke database
-                $hashed_password = password_hash($password, PASSWORD_DEFAULT);
-
                 // Persiapkan pernyataan SQL untuk menyimpan data pengguna ke dalam tabel user
-                $sql = "INSERT INTO user (email, password, firstName, lastName) VALUES (?, ?, ?, ?)";
-                $stmt = $conn->prepare($sql);
-                $stmt->bind_param("ssss", $email, $hashed_password, $firstname, $lastname);
+                $sql_id = "SELECT COUNT(userId) AS last_id FROM user";
+                $new_id1 = mysqli_query($conn,$sql_id);
+                $new_id2 = mysqli_fetch_assoc($new_id1);
+                $new_id = $firstname.$new_id2["last_id"];
 
+                $sql = "INSERT INTO user (userId,email, password, firstName, lastName) VALUES (?,?, ?, ?, ?)";
+                $stmt = $conn->prepare($sql);
+                $stmt->bind_param("sssss", $new_id, $email, $password, $firstname, $lastname);
+
+                // Hash kata sandi sebelum disimpan ke database
+                
                 // Jalankan pernyataan yang telah disiapkan
                 if ($stmt->execute()) {
+                    $sql2 = "SELECT * FROM user WHERE email='$email' AND password='$password'";
+                    $result = mysqli_query($conn, $sql2);
+                    $row = mysqli_fetch_assoc($result);
+                    $_SESSION["userId"] = $row["userId"];
+                    $_SESSION["status"] = "login";
                     // Redirect ke halaman sign-in setelah pendaftaran berhasil
                     header("Location: ../after/index.php");
                     exit();
