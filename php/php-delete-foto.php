e0f0a9d19e0918f5a0b97f07a643b6b466e78ec3
farhan <farhanangga8900@gmail.com>
2024-06-03 03:22:11 +0700

    logika delete foto ketika cancel form project

---
+++ php/php-delete-foto.php
@@ -0,0 +1,48 @@
+<?php
+if (isset($_POST["hapus-img"])) {
+    // Ambil nama file dari input POST
+    if (isset($_POST['name_file'])) {
+        $name_file = $_POST['name_file'];
+        $filename = '../asset/users/project/halaman/' . $name_file;
+
+        // Debugging: Cek path file
+        echo "Path file: $filename<br>";
+
+        // Periksa apakah file ada
+        if (file_exists($filename)) {
+            // Debugging: File ditemukan
+            echo "File ditemukan.<br>";
+
+            // Periksa izin baca dan tulis
+            if (is_readable($filename) && is_writable($filename)) {
+                // Debugging: Izin cukup
+                echo "Izin cukup untuk menghapus file.<br>";
+
+                // Hapus file
+                if (unlink($filename)) {
+
+                    echo "File berhasil dihapus.<br>";
+
+                    header("Location: ../after/index.php");
+                    exit; // Hentikan eksekusi skrip lebih lanjut
+                } else {
+                    // Debugging: Terjadi kesalahan saat menghapus file
+                    echo "Terjadi kesalahan saat menghapus file '$filename'.<br>";
+                }
+            } else {
+                // Debugging: Izin tidak mencukupi
+                echo "File '$filename' tidak memiliki izin yang diperlukan untuk dihapus.<br>";
+            }
+        } else {
+            // Debugging: File tidak ditemukan
+            echo "File '$filename' tidak ditemukan.<br>";
+        }
+    } else {
+        // Debugging: name_file tidak ada dalam POST
+        echo "Tidak ada 'name_file' dalam POST.<br>";
+    }
+} else {
+    // Debugging: hapus-img tidak di-set
+    echo "Tidak ada 'hapus-img' dalam POST.<br>";
+}
+?>
